6f9ffe1d9d5e2632a13ea0bf6cad997d64452225
nikhil <nikhilmahato3337@gmail.com>
2024-07-23 16:49:48 +0530

    location card

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -23,19 +23,21 @@ const Navbar = () => {
         ))}
       </ul>
       <div className="lg:flexCenter hidden">
-        <Button type="button" title="Login" icon="/user.svg" variant="btn_dark_green"/>
-         </div>
+        <Button
+          type="button"
+          title="Login"
+          icon="/user.svg"
+          variant="btn_dark_green"
+        />
+      </div>
 
-
-
-       <Image
-         src="menu.svg"
-         alt="menu"
-         width={32}
-         height={32}
-         className="inline-block cursor-pointer lg:hidden"
-       />
-
+      <Image
+        src="menu.svg"
+        alt="menu"
+        width={32}
+        height={32}
+        className="inline-block cursor-pointer lg:hidden"
+      />
     </nav>
   );
 };
